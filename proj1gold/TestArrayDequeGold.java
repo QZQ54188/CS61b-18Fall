d96c9c2a24de607ab192cd8ef0816c1daeb5d4e8
QZQ54188 <3375727892@qq.com>
2024-07-16 13:41:13 +0800

    proj1 gold test3

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,34 +7,6 @@ import org.junit.Test;
 public class TestArrayDequeGold {
     @Test
     public void testArrayDeque() {
-        // StudentArrayDeque<Integer> stuArrDeque = new StudentArrayDeque<>();
-        // ArrayDequeSolution<Integer> solArrDeque = new ArrayDequeSolution<>();
-        // String operation = "";
-
-        // while (true) {
-        // double rng = StdRandom.uniform();
-        // Integer temp = (int) (10 * rng);
-
-        // if (rng < 0.25) {
-        // stuArrDeque.addFirst(temp);
-        // solArrDeque.addFirst(temp);
-        // operation += "addFirst(" + temp + ")" + "\n";
-        // assertEquals(operation, solArrDeque.get(0), stuArrDeque.get(0));
-        // } else if (0.25 <= rng && rng < 0.5) {
-        // stuArrDeque.addLast(temp);
-        // solArrDeque.addLast(temp);
-        // operation += "addLast(" + temp + ")" + "\n";
-        // assertEquals(operation, solArrDeque.get(solArrDeque.size() - 1),
-        // stuArrDeque.get(stuArrDeque.size() - 1));
-        // } else if (0.5 <= rng && 0.75 > rng && solArrDeque.size() != 0) {
-        // operation += "removeFirst()" + "\n";
-        // assertEquals(operation, solArrDeque.removeFirst(),
-        // stuArrDeque.removeFirst());
-        // } else if (0.75 <= rng && solArrDeque.size() != 0) {
-        // operation += "removeLast()" + "\n";
-        // assertEquals(operation, solArrDeque.removeLast(), stuArrDeque.removeLast());
-        // }
-        // }
         StudentArrayDeque<Integer> stuArrDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solArrDeque = new ArrayDequeSolution<>();
 
